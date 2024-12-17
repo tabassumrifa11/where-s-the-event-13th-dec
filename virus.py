462d5f6b1b08563f725227df691d3cc48f7a8454
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-12-17 19:48:04 +0600

    first commit

---
+++ virus.py
@@ -0,0 +1,14 @@
+from tkinter import*
+from tkinter import messagebox
+
+root = Tk()
+root.geometry("200x200")
+
+def msg():
+    messagebox.showwarning("Alert", "stop! Virus Found")
+    
+
+button = Button(root, text="Scan for Virus", command=msg)
+
+button.place(x=40, y=80)
+root.mainloop()
